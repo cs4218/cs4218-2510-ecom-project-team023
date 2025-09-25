ba5b159f6ea45b020470f362134a0d8611f165a4
ryan <ryanlimdx@gmail.com>
2025-09-25 23:14:39 +0800

    Fix: Fix buggy imports

--- client/src/pages/user/Orders.test.js
+++ client/src/pages/user/Orders.test.js
@@ -1,7 +1,8 @@
 // Tests are written with the help of AI
 import React from "react";
-import { render, screen } from "@testing-library/react";
-import "@testing-library/jest-dom/extend-expect";
+import { render, screen, waitFor } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import axios from "axios";
 import Orders from "./Orders";
 
 // MOCKS
