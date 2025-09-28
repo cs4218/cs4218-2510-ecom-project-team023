776e8fd43753344dc3240a88b05c754bbed62f95
ryan <ryanlimdx@gmail.com>
2025-09-28 18:20:32 +0800

    Fix: Re-add selector for admin orders table - status

--- client/src/pages/admin/AdminOrders.js
+++ client/src/pages/admin/AdminOrders.js
@@ -72,7 +72,19 @@ const AdminOrders = () => {
                       <React.Fragment key={o._id}>
                         <tr className="order-summary-row">
                           <td>{i + 1}</td>
-                          <td>{o?.status}</td>
+                          <td>
+                            <Select
+                              bordered={false}
+                              onChange={(value) => handleChange(o._id, value)}
+                              defaultValue={o?.status}
+                            >
+                              {status.map((s, i) => (
+                                <Option key={i} value={s}>
+                                  {s}
+                                </Option>
+                              ))}
+                            </Select>
+                          </td>
                           <td>{o?.buyer?.name}</td>
                           <td>{moment(o?.updatedAt).fromNow()}</td>
                           <td>{o?.payment.success ? "Success" : "Failed"}</td>
@@ -113,66 +125,6 @@ const AdminOrders = () => {
           ) : (
             <p className="text-center">You haven't placed any orders yet.</p>
           )}
-          {/* {orders?.map((o, i) => {
-            return (
-              <div className="border shadow">
-                <table className="table">
-                  <thead>
-                    <tr>
-                      <th scope="col">#</th>
-                      <th scope="col">Status</th>
-                      <th scope="col">Buyer</th>
-                      <th scope="col"> date</th>
-                      <th scope="col">Payment</th>
-                      <th scope="col">Quantity</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr>
-                      <td>{i + 1}</td>
-                      <td>
-                        <Select
-                          bordered={false}
-                          onChange={(value) => handleChange(o._id, value)}
-                          defaultValue={o?.status}
-                        >
-                          {status.map((s, i) => (
-                            <Option key={i} value={s}>
-                              {s}
-                            </Option>
-                          ))}
-                        </Select>
-                      </td>
-                      <td>{o?.buyer?.name}</td>
-                      <td>{moment(o?.createAt).fromNow()}</td>
-                      <td>{o?.payment.success ? "Success" : "Failed"}</td>
-                      <td>{o?.products?.length}</td>
-                    </tr>
-                  </tbody>
-                </table>
-                <div className="container">
-                  {o?.products?.map((p, i) => (
-                    <div className="row mb-2 p-3 card flex-row" key={p._id}>
-                      <div className="col-md-4">
-                        <img
-                          src={`/api/v1/product/product-photo/${p._id}`}
-                          className="card-img-top"
-                          alt={p.name}
-                          width="100px"
-                          height={"100px"}
-                        />
-                      </div>
-                      <div className="col-md-8">
-                        <p>{p.name}</p>
-                        <p>{p.description.substring(0, 30)}</p>
-                        <p>Price : {p.price}</p>
-                      </div>
-                    </div>
-                  ))}
-                </div>
-              </div>
-            );
-          })} */}
         </div>
       </div>
     </Layout>
