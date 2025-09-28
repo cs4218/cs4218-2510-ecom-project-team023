c32d98aab451df278c611cb5a9fe9f058a2bba20
Sim Ding Hao <dinghao.sim@orangekloud.com>
2025-09-28 14:37:22 +0800

    Feat: Add unit test for SearchInput component

--- client/src/context/search.js
+++ client/src/context/search.js
@@ -2,13 +2,13 @@ import React, { useState, useContext, createContext } from "react";
 
 const SearchContext = createContext();
 const SearchProvider = ({ children }) => {
-  const [auth, setAuth] = useState({
+  const [searchValues, setSearchValues] = useState({
     keyword: "",
     results: [],
   });
 
   return (
-    <SearchContext.Provider value={[auth, setAuth]}>
+    <SearchContext.Provider value={[searchValues, setSearchValues]}>
       {children}
     </SearchContext.Provider>
   );
